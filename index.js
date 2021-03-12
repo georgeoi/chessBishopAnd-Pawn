bda7f054f02d2e2469771f5fb3c65a7e6173f9b5
georgeoi <--list>
2021-03-12 17:22:47 -0300

    resuelto

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const challengeFunction = function(bishop, pawn){
-    /* YOUR CODE HERE */
-    let result
-
-    return result
+    const letterArray= ["A","B","C","D","E","F","G","H"]
+    let arrayPosiciones=[]
+    let bishopLetter = bishop.split("")[0]
+    let bishopNumber = bishop.split("")[1]
+    letterArray.forEach(letter => {
+        let distanciaLetras = letterArray.indexOf(bishopLetter)-letterArray.indexOf(letter)
+        let newNumberPos= bishopNumber-distanciaLetras
+        let newNumberNeg= bishopNumber-distanciaLetras
+        let newPosicionPos= letter+newNumberPos
+        let newPosicionNeg= letter+newNumberNeg
+        arrayPosiciones=[...arrayPosiciones,newPosicionNeg,newPosicionPos]
+    });
+    return arrayPosiciones.indexOf(pawn)>0 
 }
-
 module.exports = challengeFunction
